eb5f61ba66d7e00490940b851e3bb57d769f91b4
TYLER SMITH <smitht93@unlv.nevada.edu>
2026-09-10 00:07:12 -0700

    My solution for Project 1 on xv6

---
+++ user/hello.c
@@ -0,0 +1,10 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(void)
+{
+    printf("Hello World!\n");
+    printf("Tyler Smith, Section 1001\n");
+    exit(0);
+}
